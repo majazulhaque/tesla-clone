f9cfe703ff89a2594931aa05acd8ce3bf8025597
Majaz <majazulhaque123@gmail.com>
2023-07-17 14:55:24 +0530

    add redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,10 +15,9 @@ function Header() {
         <img src="/images/logo.svg" alt="" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+        {cars && cars.map((car,index) =>( 
+        <a key={index} href="#">{car}</a>
+        ))}
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
@@ -29,16 +28,14 @@ function Header() {
         <CloseWrapper>
             <CustomClose onClick={()=> setBurgerStatus(false)} />
         </CloseWrapper>
+        {cars && cars.map((car,index)=>(
+          <li key={index}><a href="#">{car}</a></li>
+        ))}
         <li><a href="#">Existing Inventory</a></li>
         <li><a href="#">Used Inventory</a></li>
         <li><a href="#">Trade-in</a></li>
         <li><a href="#">Cybertruck</a></li>
         <li><a href="#">Roadaster</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
       </BurgerNav>
     </Container>
   );
