2e6b08454266cf31e554f4875495a000da10ac91
Majaz <majazulhaque123@gmail.com>
2023-07-17 11:17:43 +0530

    Add button group and down arrow with animation in cover image

--- src/components/Section.js
+++ src/components/Section.js
@@ -7,6 +7,17 @@ function Section() {
         <h1>Model S</h1>
         <p>Order Online for Touchless Delivery</p>
     </ItemText>
+    <Buttons>
+    <ButtonGroup>
+        <LeftButton>
+            Custom Order
+        </LeftButton>
+        <RightButton>
+            Existing Inventory
+        </RightButton>
+    </ButtonGroup>
+    <DownArrow src="/images/down-arrow.svg"/>
+    </Buttons>
   </Wrap>;
 }
 
@@ -20,10 +31,44 @@ const Wrap = styled.div`
   background-position:center;
   background-repeat:no-repeat;
   background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const ItemText = styled.div`
 padding-top: 15vh;
 text-align: center;
 
+`
+const ButtonGroup = styled.div`
+    display:flex;
+    margin-bottom: 30px;
+`
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+const RightButton = styled(LeftButton)`
+
+`
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+`
+const Buttons = styled.div`
+    
 `
