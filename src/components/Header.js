d58e97b5e7f5525e1a5caa1d68e8773288c0497b
Majaz <majazulhaque123@gmail.com>
2023-07-17 14:23:50 +0530

    add useState to open and close sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
   return (
     <Container>
       <a href="#">
@@ -18,11 +19,11 @@ function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={()=> setBurgerStatus(true)}/>
       </RightMenu>
-      <BurgerNav>
+      <BurgerNav show = {burgerStatus}>
         <CloseWrapper>
-            <CustomClose />
+            <CustomClose onClick={()=> setBurgerStatus(false)} />
         </CloseWrapper>
         <li><a href="#">Existing Inventory</a></li>
         <li><a href="#">Used Inventory</a></li>
@@ -94,6 +95,7 @@ const BurgerNav = styled.div`
   display: flex;
   flex-direction: column;
   text-align: start;
+  transform: ${props=> props.show ? 'translateX(0)': 'translateX(100%)'};
   li{
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, .2);
