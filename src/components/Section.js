ee3c2e62e5c96b75887f3cd5c50f3d69706418d6
Majaz <majazulhaque123@gmail.com>
2023-07-17 14:00:37 +0530

    add react-reveal to move text and button

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from 'react-reveal/Fade';
 
 function Section(props) {
   const { title, description, backgroundImg, leftBtnText, rightBtnText } =
     props;
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
+        <Fade bottom>
+        <ItemText>
         <h1>{title}</h1>
         <p>{description}</p>
       </ItemText>
+        </Fade>
+      
       <Buttons>
+        <Fade bottom>
         <ButtonGroup>
           <LeftButton>{leftBtnText}</LeftButton>
           {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
         </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
